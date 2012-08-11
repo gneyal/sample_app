5b5592a1fdc0432cbebfb1b7993b6ee395ef9d0b
Eyal <gneyal@gmail.com>
2012-08-11 16:29:53 +0300

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,10 @@ SampleApp::Application.configure do
 
   # Allow pass debug_assets=true as a query parameter to load pages with unpackaged assets
   config.assets.allow_debugging = true
+
+  # Speed up tests by lowering BCrypt's cost function.
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 end
