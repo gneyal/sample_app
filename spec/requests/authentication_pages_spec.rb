7d8e830ea7b622979feaed22f9c2f448f9ebd97e
Eyal <gneyal@gmail.com>
2012-08-13 14:59:31 +0300

    return to working state

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -33,14 +33,14 @@ describe "Authentication" do
         fill_in "Email",    with: user.email
         fill_in "Password", with: user.password
         click_button "Sign in"
-      end
+    	end
 
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
-      describe "followed by signout"
+      describe "followed by signout" do
       	before { click_link 'Sign out' }
 
       	it { should have_link 'Sign in' }
