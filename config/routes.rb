156f3d361c5c0ec407494bb773d915b7fdd83532
Eyal <gneyal@gmail.com>
2012-08-13 11:41:50 +0300

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 SampleApp::Application.routes.draw do
-  
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
   
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete # must be invoked by an HTTP DELETE request
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
